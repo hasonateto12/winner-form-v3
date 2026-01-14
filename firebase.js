a1b8674bb8dfafe0716b1e1ae191f44e5afbe26b
hasonateto12 <hassunateto3@gmail.com>
2026-01-14 18:50:01 +0200

    .

--- firebase.js
+++ firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase.js (Browser module via CDN)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAO6W2DGPwlbpplnMRrm7P1m0aF_At5gbg",
   authDomain: "winner-form.firebaseapp.com",
@@ -16,6 +12,5 @@ const firebaseConfig = {
   measurementId: "G-FZ80PD6GNH"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
